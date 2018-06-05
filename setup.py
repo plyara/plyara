8322b4e78483fdc9d7a35fa2d078cc108ea09786
rshipp <github@rshipp.com>
2018-06-04 19:08:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import sys
 
-from codecs import open
+import codecs
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 install_requires = ['ply>=3.11']
@@ -16,38 +16,25 @@ if sys.version_info < (3, ):
 
 setup(
     name='plyara',
-
-    version='1.1.1',
-
+    version='1.2.0',
     description='Parse YARA rules.',
     long_description=long_description,
-
-    url='https://github.com/8u1a/plyara',
-
+    url='https://github.com/plyara/plyara',
     author='8u1a',
-
     license='Apache License 2.0',
-
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-
         'License :: OSI Approved :: Apache Software License',
-
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
-
     keywords='malware analysis yara',
-
     py_modules=['plyara'],
-
     install_requires=install_requires,
-
     entry_points={
         'console_scripts': [
             'plyara=plyara:main',
