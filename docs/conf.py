d80e50f28f715bc571582a62b559a5f6c9777678
rshipp <github@rshipp.com>
2018-06-18 12:09:54 -0500

    Define some Alabaster config options

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,14 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_user': 'plyara',
+    'github_repo': 'plyara',
+    'github_button': 'true',
+    'github_type': 'star',
+    'description': 'Parse YARA rules into a dictionary representation.',
+    'logo_name': 'plyara',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
