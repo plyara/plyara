5cf3725b7b8f9a2d0064ee82f3aa1a01be28366c
rshipp <github@rshipp.com>
2018-08-28 17:55:33 -0500

    Bump version to v1.2.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info < (3, ):
 
 setup(
     name='plyara',
-    version='1.2.4',
+    version='1.2.5',
     description='Parse YARA rules.',
     long_description=long_description,
     url='https://github.com/plyara/plyara',
