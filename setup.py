7668d9fdb8da1a8776a26cae4fd9c44c0661a100
Malware Utkonos <utkonos@malwarolo.gy>
2019-01-15 02:55:13 -0500

    List python 3 versions in setup.py as 3.6 & 3.7

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
     keywords='malware analysis yara',
