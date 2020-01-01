350f56c7a9deb6b3641f12dee5cc12db5ad53b81
Malware Utkonos <utkonos@malwarolo.gy>
2020-01-01 18:17:52 -0500

    Remove deprecated methods in preparation for major release.

--- plyara/core.py
+++ plyara/core.py
@@ -274,54 +274,6 @@ class Parser:
 
         return self.rules
 
-    @staticmethod
-    def is_valid_rule_name(entry):
-        """Validate rule name: DEPRECATED."""
-        from plyara.utils import is_valid_rule_name
-        import warnings
-        warnings.warn('Static method is_valid_rule_name() is deprecated: use plyara.utils', DeprecationWarning)
-        return is_valid_rule_name(entry)
-
-    @staticmethod
-    def is_valid_rule_tag(entry):
-        """Validate tag: DEPRECATED."""
-        from plyara.utils import is_valid_rule_tag
-        import warnings
-        warnings.warn('Static method is_valid_rule_tag() is deprecated: use plyara.utils', DeprecationWarning)
-        return is_valid_rule_tag(entry)
-
-    @staticmethod
-    def detect_imports(rule):
-        """Detect imports: DEPRECATED."""
-        from plyara.utils import detect_imports
-        import warnings
-        warnings.warn('Static method detect_imports() is deprecated: use plyara.utils', DeprecationWarning)
-        return detect_imports(rule)
-
-    @staticmethod
-    def detect_dependencies(rule):
-        """Detect dependencies: DEPRECATED."""
-        from plyara.utils import detect_dependencies
-        import warnings
-        warnings.warn('Static method detect_dependencies() is deprecated: use plyara.utils', DeprecationWarning)
-        return detect_dependencies(rule)
-
-    @staticmethod
-    def generate_logic_hash(rule):
-        """Generate logic hash: DEPRECATED."""
-        from plyara.utils import generate_logic_hash
-        import warnings
-        warnings.warn('Static method generate_logic_hash() is deprecated: use plyara.utils', DeprecationWarning)
-        return generate_logic_hash(rule)
-
-    @staticmethod
-    def rebuild_yara_rule(rule):
-        """Rebuild rule: DEPRECATED."""
-        from plyara.utils import rebuild_yara_rule
-        import warnings
-        warnings.warn('Static method rebuild_yara_rule() is deprecated: use plyara.utils', DeprecationWarning)
-        return rebuild_yara_rule(rule)
-
 
 class Plyara(Parser):
     """Define the lexer and the parser rules."""
