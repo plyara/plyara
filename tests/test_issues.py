4ec150c51aac430838203597c08708ef2357a110
Malware Utkonos <utkonos@malwarolo.gy>
2025-01-04 03:04:05 -0500

    Happy New Year! Bumb version to 2.2.3

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,5 +1,5 @@
 # Copyright 2014 Christian Buia
-# Copyright 2024 plyara Maintainers
+# Copyright 2025 plyara Maintainers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- tests/test_linters.py
+++ tests/test_linters.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Malwarology LLC
+# Copyright 2025 Malwarology LLC
 #
 # Use of this source code is governed by an MIT-style
 # license that can be found in the LICENSE file or at
